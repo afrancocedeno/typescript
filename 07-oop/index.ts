eb3aefd35e7ebd8837fb38a725edc31875cf529e
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2023-12-11 18:45:03 -0500

    file update | my own static and readonly Math class

--- 07-oop/index.ts
+++ 07-oop/index.ts
@@ -1,3 +1,6 @@
+import { publicDecrypt } from "crypto";
+import { runInThisContext } from "vm";
+
 class Person {
     name: string = '';
     age: number = 0;
@@ -9,7 +12,7 @@ class Person {
 const person1: any = new Person();
 person1.name = 'Manuela';
 person1.age = 24;
-// // console.log(person1.greetings());
+// // // console.log(person1.greetings());
 
 /**
  * @definition - classes are object instances, with their properties you can create as many objects as you want
@@ -46,7 +49,7 @@ class Point {
 const pointA = new Point(4, 6)
 const pointB = new Point(4, 8)
 
-// console.log(Point.distance(pointA, pointB));
+// // console.log(Point.distance(pointA, pointB));
 
 class Investment {
     // public ammount: number // default is public
@@ -62,9 +65,9 @@ class Investment {
 }
 const investment = new Investment()
 investment.ammount = 3000000
-// console.log(investment);
-// console.log(investment.ammount);
-// console.log(investment.getAmmount());
+// // console.log(investment);
+// // console.log(investment.ammount);
+// // console.log(investment.getAmmount());
 
 
 /**
@@ -139,7 +142,7 @@ class MyDate {
             throw new Error('month not valid')
         }
     }
-    
+
     /**
      * @description - this is a property or attribute or method
      */
@@ -152,30 +155,142 @@ class MyDate {
 }
 
 
-const date = new MyDate(1995, 1, 31)
-console.log(date.printFormat())
-date.add(0, 'years')
-console.log(date.printFormat())
+// const date = new MyDate(1995, 1, 31)
+// // console.log(date.printFormat())
+// date.add(0, 'years')
+// // console.log(date.printFormat())
 
 
-console.log(date.printFormat())
-console.log('is leap year: ' + date.isLeapYear);
+// // console.log(date.printFormat())
+// // console.log('is leap year: ' + date.isLeapYear);
 
-date.add(1, 'years')
-console.log(date.printFormat())
-// date.isLeapYear()
-console.log('is leap year: ' + date.isLeapYear);
+// date.add(1, 'years')
+// // console.log(date.printFormat())
+// // date.isLeapYear()
+// // console.log('is leap year: ' + date.isLeapYear);
 
-date.year = 2004
+// date.year = 2004
 
-// from outside class scope I modify a public statement
+// // from outside class scope I modify a public statement
 
-console.log(date.printFormat())
-console.log('is leap year: ' + date.isLeapYear);
+// // console.log(date.printFormat())
+// // console.log('is leap year: ' + date.isLeapYear);
 
-date.month = 3
-console.log(`date set in ${date.month}`);
+// date.month = 3
+// // console.log(`date set in ${date.month}`);
 
-date.month = 0
-console.log(`date set in ${date.month}`);
+// date.month = 0
+// // console.log(`date set in ${date.month}`);
 
+/**
+ * inheritance
+ */
+class Birthday extends MyDate { }
+
+class Animal {
+
+    // constructor(public name: string) { }
+
+    // constructor(private name: string) { } // private without inheritance
+
+    constructor(public name: string) { } // internal use, not modifiable from outside
+
+    // move() { console.log('moving along!') }
+
+    greeting() { return `hello, I'm ${this.name} ` }
+
+    protected evolve() {
+        // // console.log('evolving!!')
+        this.name += ' evolving'
+    }
+
+}
+
+const juan = new Animal('Juan')
+// juan.move()
+// console.log(juan.greeting());
+
+class Dog extends Animal {
+
+    // extends properties
+    // constructor(public owner: string) {} // just like that throws an error
+    constructor(
+        // new property
+        public owner: string,
+        // inherit property defined with diferent value
+        name: string
+    ) {
+        // only call parent properties from child constructor
+        super(name)
+        // error in name duplicate with static code Analisys
+        // super.name
+        // this.name
+        // // console.log('animal name: ' + super.name);
+        // // console.log('animal name: ' + this.name);
+    }
+
+    // extends methods
+    bark(times: number) {
+        const woofs: string[] = Array.from({ length: times }, (k: string) => k = 'woof')
+
+        // woofs.forEach((e) => console.log(e))
+        this.evolve() // accesible from this subclass if parent method is protected
+    }
+    move() {
+        // console.log('\ni am a dog and: ');
+
+        // super.move()
+    }
+}
+
+const tono = new Animal('Tono')
+
+// console.log(tono);
+
+const yuta = new Dog('Alberto', 'Yuta')
+
+yuta.name // accesible from outside classes if parent class is public
+// console.log(yuta.name);
+yuta.bark(1)
+// console.log(yuta.name)
+
+// yuta.name = 'martina' // err for private and protected
+
+yuta.move()
+
+/**
+ * statics
+ */
+// console.log(Math.PI); // calling directly a math static method
+// console.log(Math.max(1, 2, 3, 4, 5, 7, 3)); // calling directly a math static method
+
+class MyMath {
+    constructor() { }
+    PI = 3.14
+}
+// console.log(MyMath.PI); // ERROR
+
+const myMathInstance = new MyMath() // current solution
+// console.log(myMathInstance.PI);
+
+class MyStaticMath {
+    constructor() { }
+    static PI = 3.14
+}
+// console.log(MyStaticMath.PI);
+MyStaticMath.PI = 2
+// console.log(MyStaticMath.PI); // ERROR it should not be modifiable
+
+class MyStaticAndReadonlyMath {
+    constructor() { }
+    static readonly PI = 3.14
+    static max(...numbers: number[]): number {
+        return numbers.reduce((max, current) => {
+            return max >= current? max: current
+        })
+    }
+}
+// console.log(MyStaticAndReadonlyMath.PI);
+// MyStaticAndReadonlyMath.PI = 2 // ERROR
+// console.log(MyStaticAndReadonlyMath.PI);
+console.log(MyStaticAndReadonlyMath.max(-1, -3, -5));
